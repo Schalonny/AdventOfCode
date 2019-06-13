85f0a926618af255a90ef1a861d6be689b863452
Łukasz Przybylak <lukasz.przybylak@gmail.com>
2019-06-13 15:35:46 +0200

    Day7 SleightInstruction - second task done, and clean code

---
+++ src/Day7/Worker.java
@@ -0,0 +1,31 @@
+package Day7;
+
+ class Worker {
+    private char currentTask;
+    private int timeToFinish;
+
+     Worker() {
+        this.timeToFinish=-1;
+        this.currentTask='-';
+    }
+
+     char getCurrentTask() {
+        return currentTask;
+    }
+
+     void setCurrentTask(char currentTask) {
+        this.currentTask = currentTask;
+    }
+
+     int getTimeToFinish() {
+        return timeToFinish;
+    }
+
+     void setTimeToFinish(int timeToFinish) {
+        this.timeToFinish = timeToFinish;
+    }
+
+     void timesFlows(){
+        this.timeToFinish--;
+    }
+}
