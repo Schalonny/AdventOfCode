94925521231aebfd2c9177a425a28ca305998d76
Łukasz Przybylak <lukasz.przybylak@gmail.com>
2019-06-14 13:22:53 +0200

    Day 8, second riddle, add missing files

--- src/Day8/NavigationSystem.java
+++ src/Day8/NavigationSystem.java
@@ -13,8 +13,9 @@ public class NavigationSystem implements Riddle {
     @Override
     public void findSolution() {
         importData();
-        calculateRootMetadata();
-        System.out.println(metadataEntries);
+        int valueOfMainRoot = calculateRootMetadata();
+        System.out.println("Sum of metadata entries is " + metadataEntries);
+        System.out.println("Sum of indicated (by metadata) metadates is " + valueOfMainRoot);
     }
 
     private void importData() {             // wypełnia tablicę DANYCH, integarami występującymi w pliku źródłowym
@@ -26,17 +27,27 @@ public class NavigationSystem implements Riddle {
     }
 
 
-    private void calculateRootMetadata() {
-        int howManyChildren = nextInt();    //odczytaj kolejną liczbę i zapisz jako liczba dzieci
-        int howManyData = nextInt();        //odczytaj kolejną liczbę i zapisz jako liczba danych
-        while (howManyChildren > 0) {       //dopóki są dzieci
-            calculateRootMetadata();        //powtórz operację na dziecku
-            howManyChildren--;              //zmniejsz liczbę dzieci
-        }                                   //po przejechaniu wszystkich dzieci, następne dane to metadane
-        while (howManyData > 0) {           //dopóki są jakieś metadane
-            metadataEntries += nextInt();   //do metadanych dodaj kolejną liczbę
-            howManyData--;                  //zmniejsz liczbę dzieci
+    private int calculateRootMetadata() {
+        int metadata = 0;                       // #zad2 - wartość metadanych tego korzenia
+        int howManyChildren = nextInt();        // odczytaj kolejną liczbę i zapisz jako liczba dzieci
+        int[] rootValuesOfChildes = new int[howManyChildren]; // #zad2 - tablica z wartościami metadanych dzieci
+        int howManyData = nextInt();            // odczytaj kolejną liczbę i zapisz jako liczba danych
+        for (int i=0;i<howManyChildren;i++) {       // iterując wszystkie dzieci
+            rootValuesOfChildes[i]=                 // #zad2 - zapisz w odpowiednim miejscu w tablicy
+                    calculateRootMetadata();        // obliczoną wartość metadanych danego dziecka
+        }                                           // #zad1 - zwiększona też została wartość metadataEntrance
+
+        while (howManyData > 0) {       //dopóki są jakieś metadane
+            int value = nextInt();      //odczytaj metadaną i zapisz w zmiennej "Value"
+            metadataEntries += value;   // #zad1 - zwiększ wartość metadataEntrance o "value"
+            if (howManyChildren==0) {   // #zad2 - jeśli dany korzeń nie ma dzieci
+                metadata+=value;        // #zad2 - to wartością jego metadanych jest wartość VALUE
+            } else if (value<=howManyChildren) {        // #zad2 - jeśli dany korzeń nie ma dzieci i VALUE to liczba wskazująca na dowolne z dzieci
+                metadata+=rootValuesOfChildes[value-1]; // #zad2 - to wartość jego metadanych zwiększ o wartość metadanych wskazanego dziecka
+            }
+            howManyData--;              //zmniejsz liczbę pozostałych dzieci do sprawdzenia
         }
+        return metadata;        // #zad2 - zwróć wartość metadanych bierzącego korzenia
     }
 
     private int nextInt() {
