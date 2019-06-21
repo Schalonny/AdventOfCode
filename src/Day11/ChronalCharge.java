c9748178c7f0670ba7822437bf60d47ee67475c5
Łukasz Przybylak <lukasz.przybylak@gmail.com>
2019-06-21 11:56:45 +0200

    Day 11, comments added

--- src/Day11/ChronalCharge.java
+++ src/Day11/ChronalCharge.java
@@ -22,6 +22,7 @@ public class ChronalCharge implements Riddle {
         fillTheGrid();
         sums.sort(Comparator.comparing(Cell::getSum3x3).reversed());
         System.out.println(sums.get(0).x+1 + "," + sums.get(0).y+1);
+        //numeracja w zadaniu jest od 1 do 300, zamiast od 0, stąd "+1"
         sums.sort(Comparator.comparing(Cell::getMaxSum).reversed());
         System.out.println(sums.get(0).maxSum);
         System.out.println(sums.get(0).x+1 + "," + sums.get(0).y+1 + "," + sums.get(0).sizeOfMaxSumSquare);
@@ -34,35 +35,46 @@ public class ChronalCharge implements Riddle {
                 int rackId = x + RACK_ID_ADDITIONAL;
                 grid[x][y] = (((rackId * (y + 1)
                         + GRID_SERIAL_NUMBER) * rackId) / 100) % 10 - 5;
+
+                // dla każdego pola na siatce wykonaj obliczenia zadane w treści
                 calculateSquaresValues(x, y);
+                // oblicz wartości dla tej komórki
             }
         }
     }
 
     private void calculateSquaresValues(int x, int y) {
-        int sum = 0;
+        int sum3x3 = 0;
         if (y < GRID_SIZE - 3 && x < GRID_SIZE - 3) {
+            // jeśli jesteśmy wystarczajaco daleko od krawędzi by zmieścić kwadrat 3x3
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    sum += grid[x + i][y + j];
+                    sum3x3 += grid[x + i][y + j];
+                    // jako sumę ustaw zsumowaną wartość tych 9 liczb z kwadratu
                 }
             }
         }
-        sums.add(setMaxSumParameters(new Cell(x, y, sum)));
+        sums.add(setMaxSumParameters(new Cell(x, y, sum3x3)));
     }
 
     private Cell setMaxSumParameters(Cell cell){
         cell.maxSum = cell.sum3x3;
-        for (int i=4; i<GRID_SIZE-cell.x && i<GRID_SIZE-cell.y; i++){
+        // zakładamy, że dla danej komórki jej największą watrtością jest wartość z kwadratu 3x3
+        // co nie musi być koniecznie prawdą, ale interesują nas znacznie większe wyniki
+        for (int sizeOfSquare=4; sizeOfSquare<GRID_SIZE-cell.x && sizeOfSquare<GRID_SIZE-cell.y; sizeOfSquare++){
+            // dopóki wielkość sprawdzanego kwadratu pozostaje w wymiarach siatki
             int sum = 0;
-            for (int x = 0; x < i; x++) {
-                for (int y = 0; y < i; y++) {
+            for (int x = 0; x < sizeOfSquare; x++) {
+                for (int y = 0; y < sizeOfSquare; y++) {
                     sum += grid[cell.x + x][cell.y + y];
+                    //zsumuj wszystkie wartości z badanego kwadratu
                 }
             }
             if (sum>cell.maxSum){
-                cell.sizeOfMaxSumSquare = i;
+                // jeśli obliczona suma jest większa niż dotychczasowa maxSum
+                cell.sizeOfMaxSumSquare = sizeOfSquare;
                 cell.maxSum = sum;
+                // to odpowiedniu ustaw wartości
             }
         }
         return cell;
